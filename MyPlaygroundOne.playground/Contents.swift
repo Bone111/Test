26f1e37f1cc8cf49aa01185fdde465161c17a9f0
Bone Lee <lzp111@163.com>
2017-06-06 16:01:46 +0800

    提交本地变更

---
+++ MyPlaygroundOne.playground/Contents.swift
@@ -0,0 +1,98 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var time = "time"
+
+let b = 9 + 1
+
+
+typealias Feet = Int
+
+var distance: Feet = 6785578
+
+print(distance)
+
+print(distance, separator: "8")
+
+
+typealias NSString = String
+
+var name: NSString = "alian "
+
+
+var pi = 3.1415926
+
+pi = 1
+
+var 你是谁 = "主席"
+
+print(你是谁)
+
+print(你是谁, terminator: "")
+
+print("\(name)hello:\(你是谁)")
+
+var optinal12: Int?
+
+optinal12
+
+
+var mystring:String? = "21212"
+
+
+
+if mystring != nil {
+    print(mystring!)
+}else{
+    print("字符串为 nil")
+}
+
+
+
+
+var  autoString:String? = "2112"
+
+autoString = "qwwqw"
+
+if autoString != nil {
+    print(autoString)
+}else{
+    print("nil")
+}
+
+var yourString:String?
+
+yourString = "hello,swift"
+
+if let herString = yourString  {
+    print("\(herString)")
+}else{
+    print("no string")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
